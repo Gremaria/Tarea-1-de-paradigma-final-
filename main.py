10c75c03a6f364536a3814208adc112767547eac
Teo <tcoz@miuandes.cl>
2026-03-23 17:23:42 -0300

    Final

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from io_handler import cargar_trabajo, cargar_recursos, tiempo_cronograma, tabla_cronograma
-
+from scheduler import tiempos_cronograma, compatibilidad
+import json
 def main():
-    # 1. Pedir archivos
+
     tareas_path = input("Archivo de tareas: ")
     recursos_path = input("Archivo de recursos: ")
 
-    # 2. Cargar datos
     tareas = cargar_trabajo(tareas_path)
     recursos = cargar_recursos(recursos_path)
+    print("hoalnsdaksndkan")
+    resultado = tiempos_cronograma(recursos, tareas)
 
-    # 3. Generar cronograma
-    resultado = tiempo_cronograma(tareas, recursos)
-
-    # 4. Mostrar resultados
     print("\nTiempo de ejecución:", resultado["tiempo_ms"], "segundos")
 
-    # 5. Mostrar tabla bonita
     tabla_cronograma(resultado)
+    opcion = input("\n¿Guardar cronograma? (s/n): ")
 
+    if opcion.lower() == "s":
+        nombre = input("Nombre del archivo (ej: output.json): ")
+    
+        with open(nombre, "w") as f:
+            json.dump(resultado["lista"], f, indent=4)
+    
+        print(f"Cronograma guardado en {nombre}")
 
 if __name__ == "__main__":
     main()
